7cc9a3319bd1ab744a71cd5eee37b4f73504c9c6
Claudwin <claudwin.fortune@gmail.com>
2019-01-16 22:25:47 +0000

    Added payment processing with stripe payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_user_enrollment
+
     def show
         
     end 
     
     private
+  def require_user_enrollment
+    if  !current_user.enrolled_in?(current_lesson.section.course)  
+      redirect_to course_url(current_lesson.section.course), alert: 'You must enroll to view lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
